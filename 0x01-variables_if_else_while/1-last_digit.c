82ae2a079ebe35891cc9c38d4a30f2ebbd77a85e
Lmbiu85 <levymbiu@gmail.com>
2023-03-17 13:37:57 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * main - prints a text according number
+ * Return: Always 0 (succes)
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(00));
+	n = rand() - RAND_MAX / 2;
+	/* my code goes in there*/
+	return (0);
+}
