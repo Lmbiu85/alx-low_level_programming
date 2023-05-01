750b6445c7115d7da065903d831ac1321f3e4cde
Lmbiu85 <levymbiu@gmail.com>
2023-05-01 12:22:53 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,16 +10,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t x = 0;
-
-	if (h == NULL)
-	{
-		return (0);
-	}
-	while (h != NULL)
+	
+	while (h)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
 		x++;
+		h=h->next;
 	}
 	return (x);
 }
